9e6bc8169596f45496beed5a1f3eb15dd1860bd3
Jason Kwon <jasonproject628@gmail.com>
2022-10-24 06:42:51 +0900

    NEW MEMBER SERVICE

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,16 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // MemoryMemberRepository 로 만들어서 가입, 회원조회를 구현
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
