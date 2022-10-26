025f2b3ef1e5d2b8360f3d4f8edd16f86d24c615
Jason Kwon <jasonproject628@gmail.com>
2022-10-27 06:07:28 +0900

    Dependency Injection, AppConfig

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     // MemoryMemberRepository 로 만들어서 가입, 회원조회를 구현
     @Override
     public void join(Member member) {
