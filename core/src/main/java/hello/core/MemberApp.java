296946742035b9ba2ac6e5d0123a7bf1789622b5
Jason Kwon <jasonproject628@gmail.com>
2022-10-29 07:41:17 +0900

    ApplicationContext, Spring Bean Container

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -1,32 +1,28 @@
 package hello.core;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.order.Order;
-import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class OrderApp {
-
+public class MemberApp {
     public static void main(String[] args) {
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
-//        OrderService orderService = appConfig.orderService();
 
+        // Spring 컨테이너 시작 시 항상 쓰는 것, Annotation 기반으로 Config 하는 것을 설정 해줌 (AppConfig 클래스에 있는 Annotation)
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
-        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
-        Long memberId = 1L;
-        Member member = new Member(memberId, "memberA", Grade.VIP);
+        //MemberService 클래스 내의 memberService 객체를 불러온다
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 20000);
-
-        System.out.println("order = " + order);
+        Member findmember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findmember.getName());
     }
 }
